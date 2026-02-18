8da23ed62aecc366595ae4413e6d8bc69800544e
Neyr <neyrworks@gmail.com>
2026-02-18 16:40:38 +0700

    upd: add components and adding new routes for dashboard

---
+++ resources/views/components/navLink.blade.php
@@ -0,0 +1,3 @@
+@props(['link'])
+
+<a href="{{ $link }}" class="text-sm/6 font-medium hover:text-white text-white/90">{{$slot}}</a>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.mainLayout')
+
+@section('content')
+<div class='min-h-screen -full flex items-center text-center justify-center flex-col gap-20'>
+    <p class="text-8xl font-bold mono">Edutrack</p>
+    <div class="space-y-4 flex items-center justify-center flex-col">
+        <h1 class='text-3xl font-medium w-4xl'><span class="text-blue-500 font-bold">Bonjour</span>, I'm Edutrack. <br/> say hi to your favorite activity management!</h1>
+        <h2 class='text-xs w-md'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, totam? Voluptates eum ea similiqu</h2>
+    </div>
+</div>
+
+@endsection
